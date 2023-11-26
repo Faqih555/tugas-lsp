6352163469a619ab8718812124389c6fc1d25bdf
faqih555 <m.faqihabdilah@gmail.com>
2023-11-26 22:43:55 +0800

    selesai

--- app.js
+++ app.js
@@ -1,28 +1,113 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const hostname = '127.0.0.1';
+const http = require("http");
+const fs = require("fs");
+const querystring = require("querystring");
+const path = require("path"); 
+const url = require("url");  
+const users = require('./user'); // Import array pengguna dari file users.js
+
+const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  // Membaca path dari URL
-  const url = req.url === '/' ? '/index.html' : req.url;
-  const filePath = path.join(__dirname, 'public', url);
+http.createServer(async (req, res) => {
+  const reqUrl = req.url;
+  const method = req.method;
 
-  // Membaca file HTML
-  fs.readFile(filePath, 'utf8', (err, content) => {
-    if (err) {
-      // Jika file tidak ditemukan, kirimkan respons 404 Not Found
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Not Found\n');
-    } else {
-      // Jika file ditemukan, kirimkan respons dengan isi file HTML
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(content);
+  console.log(reqUrl, method);
+
+  if (reqUrl === "/") {
+    if (method === 'GET') {
+      // Tampilkan halaman login
+      fs.readFile(`./public/login.html`, (err, data) => {
+        if (err) {
+          res.writeHead(404);
+          res.end('Error: File not found');
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+        }
+        res.end();
+      });
+    } else if (method === 'POST') {
+      // Tangani data login yang dikirim melalui POST
+      let body = '';
+      req.on('data', chunk => {
+        body += chunk.toString();
+      });
+      req.on('end', () => {
+        const formData = querystring.parse(body);
+        const username = formData.name;
+        const password = formData.password;
+
+        // Validasi login dengan array pengguna
+        const user = users.find(u => u.username === username && u.password === password);
+
+        if (user) {
+          // Redirect ke halaman home jika login berhasil
+          res.writeHead(302, { 'Location': '/home' });
+          console.log('login berhasil')
+        } else {
+          // Redirect kembali ke halaman login jika login gagal
+          res.writeHead(302, { 'Location': '/' });
+          console.log('login gagal')
+        }
+        res.end();
+      });
     }
-  });
-});
+  } else if (reqUrl.startsWith("/transaction")) {
+    const parsedUrl = url.parse(reqUrl, true);  // Parse URL dengan parameter query
+    const { pathname, query } = parsedUrl;
 
-server.listen(port, hostname, () => {
+    if (pathname === "/transaction" && method === "GET") {
+      // Menampilkan halaman transaksi
+      fs.readFile(`./public/transaction.html`, (err, data) => {
+        if (err) {
+          res.writeHead(404);
+          res.end('Error: File not found');
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+        }
+        res.end();
+      });
+    } else {
+      res.writeHead(404);
+      res.end("Error: Page not found");
+    }
+  } else if (reqUrl === "/home") {
+    fs.readFile(`./public/index.html`, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end('Error: File not found');
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+      }
+      res.end();
+    });
+  } else if (reqUrl === "/image/logo") {
+    fs.readFile('./public/assets/logo.png', (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end("Error: File not found");
+      } else {
+        res.writeHead(200, { "Content-Type": "image/png" });
+        res.end(data);
+      }
+    });
+  } else if (reqUrl === "/image/banner") {
+    fs.readFile('./public/assets/download.jpeg', (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end("Error: File not found");
+      } else {
+        res.writeHead(200, { "Content-Type": "image/jpeg" });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end("Error: Page not found");
+  }
+}).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
---
+++ user.js
@@ -0,0 +1,8 @@
+// File: users.js
+const users = [
+    { username: 'user1', password: 'pass1' },
+    { username: 'user2', password: 'pass2' },
+    // Tambahkan pengguna lain jika diperlukan
+];
+
+module.exports = users;
